05b0b375a9279b1d63f8072d4d1756f30306457b
lkno0705 <lkno0705@gmail.com>
2022-05-28 12:00:26 +0200

    Add Documentation, Rollback Dependency Rule breaking Example from earlier in the project

--- 2-dnd-charactermanager-application/src/main/java/repositorys/RepositoryService.java
+++ 2-dnd-charactermanager-application/src/main/java/repositorys/RepositoryService.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public final class RepositoryService {
 
-    private final RPGCharacterRepository rpgCharacterRepository;
-    private final RPGCharacterGraveyard rpgCharacterGraveyard;
+    private final RPGCharacterRepositoryImpl rpgCharacterRepository;
+    private final RPGCharacterGraveyardImpl rpgCharacterGraveyard;
 
-    public RepositoryService(RPGCharacterRepository rpgCharacterRepository, RPGCharacterGraveyard rpgCharacterGraveyard) {
+    public RepositoryService(RPGCharacterRepositoryImpl rpgCharacterRepository, RPGCharacterGraveyardImpl rpgCharacterGraveyard) {
         this.rpgCharacterRepository = rpgCharacterRepository;
         this.rpgCharacterGraveyard = rpgCharacterGraveyard;
     }
